d2e70dece53e5d39a7aa7c24255901de75986b1e
Venelin Manchev <manchev@prodio.bg>
2017-02-01 00:19:16 +0200

    updated gulp.watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,23 @@ var wiredep = require('gulp-wiredep');
 var inject = require('gulp-inject');
 
 gulp.task('inject', function () {
-  gulp.src('./app/layout/index.html')
-          .pipe(wiredep())
-          .pipe(inject(gulp.src('./app/**/*', {read: false})))
-          .pipe(gulp.dest('./public'));
+  gulp.src('app/layout/index.html')
+      .pipe(wiredep())
+      .pipe(
+        inject(
+          gulp.src('app/**/!(*\.spec)*', {read: false}),
+          {
+            addPrefix: '../..',
+            addRootSlash: false
+          }
+        )
+      )
+      .pipe(gulp.dest('public'));
 });
 
 
 gulp.task('watch', function () {
-  gulp.watch('./app/**/*', ['inject']);
+  gulp.watch('app/**/!(*\.spec)*', ['inject']);
 });
 
 gulp.task('default', ['inject', 'watch']);
